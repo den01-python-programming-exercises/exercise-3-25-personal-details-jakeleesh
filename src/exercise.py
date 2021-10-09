b3efbe3d1939d4e5887b8ab142c807384911b9c2
Jake Lee <jake.lee@fdmgroup.com>
2021-10-09 16:33:46 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,23 @@
 def main():
     #write your code below this line
+    sum = 0
+    count = 0
+    longest = 0
+    long_name = ""
+    while True:
+        details = input()
+        if (details != ""):
+            details_split = details.split(",")
+            sum += int(details_split[1])
+            count += 1
+            if (len(details_split[0]) > longest):
+                longest = len(details_split[0])
+                long_name = details_split[0]
+        else:
+            break
+    avg = sum / count
+    print("Longest name: " + long_name)
+    print("Average of the birth years: " + str(avg))
 
 if __name__ == '__main__':
     main()
